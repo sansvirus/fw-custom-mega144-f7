bb1cd395ffbc29feba8504f7a9475b3730e2a4b1
rusefillc <rusefillc@gmail.com>
2025-08-12 18:00:04 -0400

    .yaml

---
+++ M144_meta.h
@@ -0,0 +1,211 @@
+#pragma once
+// D0 A10
+#define M144_D0 A10
+
+// D1 A9
+#define M144_D1 A9
+
+// D2 B15
+#define M144_D2 B15
+
+// D3 B14
+#define M144_D3 B14
+
+// D4 D8
+#define M144_D4 D8
+
+// D5 D9
+#define M144_D5 D9
+
+// D6 D10
+#define M144_D6 D10
+
+// D7 D11
+#define M144_D7 D11
+
+// D8 C6
+#define M144_D8 C6
+
+// D9 C7
+#define M144_D9 C7
+
+// D10 C8
+#define M144_D10 C8
+
+// D11 C9
+#define M144_D11 C9
+
+// D12 A8
+#define M144_D12 A8
+
+// D13 A15
+#define M144_D13 A15
+
+// D14 B10
+#define M144_D14 B10
+
+// D15 B11
+#define M144_D15 B11
+
+// D16 D5
+#define M144_D16 D5
+
+// D17 D6
+#define M144_D17 D6
+
+// D18 D4
+#define M144_D18 D4
+
+// D19 D3
+#define M144_D19 D3
+
+// D20 E13
+#define M144_D20 E13
+
+// D21 E12
+#define M144_D21 E12
+
+// D22 E11
+#define M144_D22 E11
+
+// D23 E10
+#define M144_D23 E10
+
+// D24 E15
+#define M144_D24 E15
+
+// D25 E9
+#define M144_D25 E9
+
+// D26 E8
+#define M144_D26 E8
+
+// D27 E7
+#define M144_D27 E7
+
+// D28 B2
+#define M144_D28 B2
+
+// D29 D15
+#define M144_D29 D15
+
+// D30 D14
+#define M144_D30 D14
+
+// D31 B13
+#define M144_D31 B13
+
+// D32 D12
+#define M144_D32 D12
+
+// D33 D13
+#define M144_D33 D13
+
+// D34 B12
+#define M144_D34 B12
+
+// D35 D7
+#define M144_D35 D7
+
+// D36 G14
+#define M144_D36 G14
+
+// D37 E14
+#define M144_D37 E14
+
+// D38 G15
+#define M144_D38 G15
+
+// D39 B4
+#define M144_D39 B4
+
+// D40 B3
+#define M144_D40 B3
+
+// D41 B5
+#define M144_D41 B5
+
+// D42 B7
+#define M144_D42 B7
+
+// D43 B6
+#define M144_D43 B6
+
+// D44 B9
+#define M144_D44 B9
+
+// D45 B8
+#define M144_D45 B8
+
+// D46 E0
+#define M144_D46 E0
+
+// D47 E1
+#define M144_D47 E1
+
+// D48 E2
+#define M144_D48 E2
+
+// D49 E3
+#define M144_D49 E3
+
+// D50 E4
+#define M144_D50 E4
+
+// D51 E5
+#define M144_D51 E5
+
+// D52 E6
+#define M144_D52 E6
+
+// D53 C13
+#define M144_D53 C13
+
+// A0 EFI_ADC_0
+#define M144_A0 EFI_ADC_0
+
+// A1 EFI_ADC_1
+#define M144_A1 EFI_ADC_1
+
+// A2 EFI_ADC_2
+#define M144_A2 EFI_ADC_2
+
+// A3 EFI_ADC_3
+#define M144_A3 EFI_ADC_3
+
+// A4 EFI_ADC_4
+#define M144_A4 EFI_ADC_4
+
+// A5 EFI_ADC_5
+#define M144_A5 EFI_ADC_5
+
+// A6 EFI_ADC_6
+#define M144_A6 EFI_ADC_6
+
+// A7 EFI_ADC_7
+#define M144_A7 EFI_ADC_7
+
+// A8 EFI_ADC_8
+#define M144_A8 EFI_ADC_8
+
+// A9 EFI_ADC_9
+#define M144_A9 EFI_ADC_9
+
+// A10 EFI_ADC_10
+#define M144_A10 EFI_ADC_10
+
+// A11 EFI_ADC_11
+#define M144_A11 EFI_ADC_11
+
+// A12 EFI_ADC_12
+#define M144_A12 EFI_ADC_12
+
+// A13 EFI_ADC_13
+#define M144_A13 EFI_ADC_13
+
+// A14 EFI_ADC_14
+#define M144_A14 EFI_ADC_14
+
+// A15 EFI_ADC_15
+#define M144_A15 EFI_ADC_15
+
