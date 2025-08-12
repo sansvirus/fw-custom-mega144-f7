281676909be18c0cc66ab1909dccf343138b650e
rusefillc <rusefillc@gmail.com>
2025-08-12 17:43:28 -0400

    customBoardDefaultConfiguration: ADC, SPI3

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -13,35 +13,12 @@ Gpio getWarningLedPin() {
 	return Gpio::Unassigned;
 }
 
-// board-specific configuration setup
 static void customBoardDefaultConfiguration() {
-    // engineConfiguration->injectionPins[0] = Gpio::F13;
-    // engineConfiguration->ignitionPins[0] = Gpio::E15;
+    engineConfiguration->analogInputDividerCoefficient = 1.47f;
 
-//   engineConfiguration->triggerInputPins[0] = Gpio::B1;
-//	engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
+	engineConfiguration->adcVcc = 3.3f;
+	engineConfiguration->is_enabled_spi_3 = true;
 
-//	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
-
-//	engineConfiguration->clt.adcChannel = EFI_ADC_1;
-
-//	engineConfiguration->iat.adcChannel = EFI_ADC_2;
-
-
-    	// 5.6k high side/10k low side = 1.56 ratio divider
-  //  	engineConfiguration->analogInputDividerCoefficient = 1.56f;
-
-    	// 6.34k high side/ 1k low side
-//    	engineConfiguration->vbattDividerCoeff = (6.34 + 1) / 1;
-
-//	engineConfiguration->adcVcc = 3.3f;
-
-//	engineConfiguration->clt.config.bias_resistor = 2490;
-//	engineConfiguration->iat.config.bias_resistor = 2490;
-
-
-	// Battery sense on PA0
-//	engineConfiguration->vbattAdcChannel = EFI_ADC_0;
 }
 
 void setup_custom_board_overrides() {
