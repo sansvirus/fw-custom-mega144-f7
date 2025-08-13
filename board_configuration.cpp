40d12e049b1f0381a7026e8fe4f6f76ac8700728
sansvirus <129865458+sansvirus@users.noreply.github.com>
2025-08-13 07:25:33 +0200

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -23,4 +23,9 @@ static void customBoardDefaultConfiguration() {
 
 void setup_custom_board_overrides() {
     custom_board_DefaultConfiguration = customBoardDefaultConfiguration;
+	setHellenMegaEnPin();
+	hellenMegaSdWithAccelerometer();
+	setHellenCan();
+ 	setDefaultHellenAtPullUps();
+    setHellenMMbaro();
 }
