053d53067003e5df448143c6d3c99a7c02090fa3
sansvirus <129865458+sansvirus@users.noreply.github.com>
2025-08-13 07:47:10 +0200

    Update board.mk

--- board.mk
+++ board.mk
@@ -8,6 +8,3 @@ include $(BOARD_DIR)/meta-info.env
 
 # this would save some flash while being unable to update WBO controller firmware
 DDEFS += -DEFI_WIDEBAND_FIRMWARE_UPDATE=FALSE
-
-# assign critical LED to a non-existent pin
-DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
